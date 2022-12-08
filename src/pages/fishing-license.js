97dc274db540b9413df5b0d2d070973ee17e00a6
Isaac Tait <isaac@mountaintopcoding.com>
2022-12-08 14:40:41 -0500

    feat:update fishing license page 😇

--- src/pages/fishing-license.js
+++ src/pages/fishing-license.js
@@ -63,25 +63,27 @@ const License = ({ location }) => {
             <header class="pr-2 md:w-2/3">{header}</header>
             <div class="bg-neutral-100 h-screen mb-4 w-full lg:w-2/3 mx-auto overflow-hidden rounded-lg shadow-xl">
               <p class="ml-2 mb-4">Obtaining a fishing license (aka hituri ken – 日釣 券 ) in Japan can be easy, as long as you know how to go about acquiring one.</p>
-              <p class="ml-2 mb-4">First make sure the river is actually open for fishing. For more information seeing Keiryu Fishing Season.</p>
+              <p class="ml-2 mb-4">First make sure the river is actually open for fishing. For more information seeing&nbsp;<a 
+                href="/keiryu-fishing-season" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                class="text-red-500 hover:bg-red-500 hover:text-white">Keiryu Fishing Season</a>.</p>
               <p class="ml-2 mb-4">Next you need to find the place to acquire your hituri ken. Sometimes they are sold at convenience stores other times you buy them from a co-op volunteers home.</p>
               <p class="ml-2 mb-4">Keep your eyes peeled for a yellow flag called yūryōken hanbaisyo (遊漁券 販売所).</p>
               <p class="ml-2 mb-4">Before purchasing a hituri ken you need to make sure it is for the section of the river you want to fish. Often times a river can be “owned” by several co-ops or Fisheries Cooperative Associations (aka 内水面漁業協同組合). For each section of the river you will need to buy a separate hituri ken.</p>
               <p class="ml-2 mb-4">A hituri ken typically cost anywhere from ¥500 – ¥4,400 per person per day and often have a restriction on how many fish you can keep.</p>     
               <p class="ml-2 mb-4">Only rivers that are stocked by Fisheries Cooperative Association require a hituri ken. If you are fishing genryu and keiryu deep in the mountains you typically do not need one.</p>
               <p class="ml-2 mb-4">If you are supposed to have a hituri ken but do not know where to purchase one, do not despair often times you can buy them on the spot when asked by a volunteer.</p>
-              <p class="ml-2 mb-4">Helpful sites: 
-                <a 
+              <p class="ml-2 mb-4">Helpful sites:&nbsp;<a 
                   href="https://www.tsuritickets.com/manual" 
                   target="_blank" rel="noopener noreferrer"
-                  class="ml-4 external link font-bold text-red-500 hover:bg-red-500 hover:text-white"
-                >Tsuri Tickets</a> and 
-                <a 
+                  class="font-bold text-red-500 hover:bg-red-500 hover:text-white"
+                >Tsuri Tickets</a>&nbsp;and&nbsp;<a 
                   href="https://www.satofull.jp/products/detail.php?product_id=1001064" 
                   target="_blank" 
                   rel="noopener noreferrer"
-                  class="ml-4 external link font-bold text-red-500 hover:bg-red-500 hover:text-white"
-                >Satofull</a></p>
+                  class="font-bold text-red-500 hover:bg-red-500 hover:text-white"
+                >Satofull</a>.</p>
             </div>
             <Footer />
         </div>
